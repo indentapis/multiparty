d44322f27dd4a1d5d4e25adf22c71ed6b7584f59
Dan Gillespie <dan@indent.com>
2023-09-29 13:04:53 -0400

    fix ci

--- scripts/get.sh
+++ scripts/get.sh
@@ -5,24 +5,32 @@ set -euo pipefail
 SUMS="$(dirname "$(readlink -f "$0")")/get.sha256"
 OUTDIR="$(dirname "$(readlink -f "$0")")/../_proto"
 
-arch="$(uname -m)"
-kernel="$(uname -s)"
-kernelLower="$(tr '[:upper:]' '[:lower:]' <<< "$kernel")"
-
-os="$kernelLower"
-if [[ "$kernelLower" == "darwin" ]]; then
-  os="osx"
-fi
-
 # write to temp file and cleanup later
 tmpDir="$(mktemp -d)"
 function cleanup() {
-	rm -rf ${tmpDir}
+	rm -rf "$tmpDir"
 }
 trap cleanup EXIT
 
 function main() {
+  local arch
+  local kernel
+  local kernelLower
+  local os
+
   for name in "$@"; do
+    # computing environment
+    arch="$(uname -m)"
+    kernel="$(uname -s)"
+    kernelLower="$(tr '[:upper:]' '[:lower:]' <<< "$kernel")"
+
+    os="$kernelLower"
+    if [[ "$kernelLower" == "darwin" ]]; then
+      os="osx"
+    fi
+
+    # install
+    mkdir -p "$OUTDIR"
     case $name in
       buf)
         version=1.26.1
@@ -52,13 +60,16 @@ function main() {
         if [[ "$kernelLower" == "darwin" ]]; then
           os="darwin"
         fi
+        if [[ "$arch" == "x86_64" ]]; then
+          arch="amd64"
+        fi
 
         version=v1.31.0
         target="protoc-gen-go.${version}.${os}.${arch}.tar.gz"
         url="https://github.com/protocolbuffers/protobuf-go/releases/download/${version}/${target}"
         download "$url" "$tmpDir/$target"
 
-        tar xvf "$tmpDir/$target" -C "${OUTDIR}" protoc-gen-go
+        tar xvf "$tmpDir/$target" -C "$OUTDIR" protoc-gen-go
         ;;
 
       *)
@@ -68,7 +79,6 @@ function main() {
 
     esac
     echo "got ${name}!"
-
   done
 }
 
@@ -81,28 +91,28 @@ function download() {
 		exit 3
 	elif [[ -f ${dst} ]]; then
 		# check if already have copy
-		check ${dst} && exit 0
+		check "$dst" && exit 0
 	fi
 
 	echo "Downloading ${url}..."
-	curl -L -o ${dst} "${url}"
+	curl -L -o "$dst" "$url"
 
-	echo "Verifying checksum of ${dst}"
-	check ${dst} || (echo "Failed checksum!" && exit 2)
+	check "$dst" || (echo "Failed checksum!" && exit 2)
 	echo "Checksum passed!"
 }
 
 function check() {
   local target="${1}"
-	local checksum=$(grep $(basename ${target}) ${SUMS})
+	local checksum
+	checksum=$(grep "$(basename "$target")" "$SUMS")
 
   # if no checksum provided, add to file
 	if [[ -z ${checksum} ]]; then
-    checksum=$(cd $(dirname ${target}) && sha256sum $(basename ${target}))
+    checksum=$(cd "$(dirname "$target")" && sha256sum "$(basename "$target")")
 
     if [[ -t 0 ]]; then
-      printf "Checksum not found for ${target}.\nChecksum: ${checksum}\n"
-      read -p "Do you want to add it? (Y/n) " response
+      printf "Checksum not found for %s.\nChecksum: %s\n" "$target" "$checksum"
+      read -r -p "Do you want to add it? (Y/n) " response
       case ${response} in
         [nN]|[nN][oO]) return 3 ;;
       esac
@@ -111,11 +121,13 @@ function check() {
     fi
 
     echo "Adding checksum: ${checksum}"
-    local newSums=$(echo ${checksum} | cat ${SUMS} - | sort -k2 | uniq)
-    echo "${newSums}" > ${SUMS}
+    local newSums
+    newSums=$(echo "$checksum" | cat "$SUMS" - | sort -k2 | uniq)
+    echo "$newSums" > "$SUMS"
 	fi
 
-	cd $(dirname ${target}) && sha256sum --check --status <<< "${checksum}"
+  echo "Verifying ${checksum}"
+	cd "$(dirname "$target")" && sha256sum --check --status <<< "$checksum"
 }
 
 main "$@"
