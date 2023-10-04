d837c29cb194583736b136d4d06e79eed03e4201
Dan Gillespie <dan@indent.com>
2023-10-04 11:33:58 -0400

    remove googleapis submodule

--- proto.mk
+++ proto.mk
@@ -16,15 +16,11 @@ $(PKGS): proto-deps
 
 proto-deps: \
  $(PROTO)/buf \
- $(PROTO)/googleapis \
  $(PROTO)/protoc/bin/protoc \
  $(PROTO)/protoc-gen-go
 
 $(PROTO)/buf:
 	@$(GET) buf
-
-$(PROTO)/googleapis:
-	@git submodule update --quiet --depth=1 --init $@ >/dev/null 2>&1
 
 $(PROTO)/protoc/bin/protoc:
 	@$(GET) protoc
